80e016bd8fbbf28a35f7ac9f1d9bfa56ccd7c54f
Comandeer <vepomoc@gmail.com>
2019-12-24 00:03:12 +0100

    feat(reporter): add basic reporter

---
+++ tests/reporter.js
@@ -0,0 +1,67 @@
+import { expect } from 'chai';
+import { use as chaiUse } from 'chai';
+import { spy } from 'sinon';
+import sinonChai from 'sinon-chai';
+import reporter from '../src/reporter.js';
+
+chaiUse( sinonChai );
+
+describe( 'reporter', () => {
+	it( 'is a function', () => {
+		expect( reporter ).to.be.a( 'function' );
+	} );
+
+	it( 'requires array of reporters', () => {
+		expect( () => {
+			reporter( [
+				{
+					results: {},
+					reporter() {}
+				},
+
+				{
+					results: {}
+				}
+			] );
+		} ).to.throw( TypeError, 'Passed results must be of correct type' );
+
+		expect( () => {
+			reporter( [
+				{
+					results: {},
+					reporter() {}
+				},
+
+				{
+					results: {},
+					reporter() {}
+				}
+			] );
+		} ).not.to.throw( TypeError, 'Passed results must be of correct type' );
+	} );
+
+	it( 'correctly renders the report', () => {
+		const step1Results = {};
+		const step1Reporter = spy();
+		const step2Results = {};
+		const step2Reporter = spy();
+
+		const steps = [
+			{
+				results: step1Results,
+				reporter: step1Reporter
+			},
+
+			{
+				results: step2Results,
+				reporter: step2Reporter
+			}
+		];
+
+		reporter( steps );
+
+		expect( step1Reporter ).to.have.been.calledOnceWithExactly( step1Results );
+		expect( step2Reporter ).to.have.been.calledOnceWithExactly( step2Results );
+		expect( step2Reporter ).to.have.been.calledImmediatelyAfter( step1Reporter );
+	} );
+} );
