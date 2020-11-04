789c5d85c0f070f0124a4b4015c280d2fe3498d5
Comandeer <vepomoc@gmail.com>
2020-11-04 01:14:06 +0100

    test(runner): add tests for events

--- tests/Runner.js
+++ tests/Runner.js
@@ -1,13 +1,20 @@
 /* globals expect, sinon */
 
+import EventEmitter from 'events';
 import assertParameter from './helpers/assertParameter.js';
 import Runner from '../src/Runner.js';
 
-const { stub } = sinon;
+const { spy, stub } = sinon;
 
 describe( 'Runner', () => {
 	it( 'is a class', () => {
 		expect( Runner ).to.be.a( 'function' );
+	} );
+
+	it( 'is an event emitter', () => {
+		const runner = new Runner();
+
+		expect( runner ).to.be.an.instanceOf( EventEmitter );
 	} );
 
 	describe( '#steps', () => {
@@ -397,6 +404,163 @@ describe( 'Runner', () => {
 			expect( stub1 ).to.have.been.calledOnce;
 			expect( stub2 ).to.have.been.calledOnce;
 			expect( stub3 ).not.to.have.been.called;
+		} );
+	} );
+
+	describe( 'events', () => {
+		it( 'emits events in correct order', async () => {
+			const runner = new Runner();
+			const step = {
+				name: 'Step #1',
+				run() {
+					return {
+						results: {
+							ok: true
+						},
+						reporter() {}
+					};
+				}
+			};
+
+			runner.addStep( step );
+
+			const startListener = spy();
+			const stepStartListener = spy();
+			const stepEndListener = spy();
+			const endListener = spy();
+
+			runner.on( 'start', startListener );
+			runner.on( 'step:start', stepStartListener );
+			runner.on( 'step:end', stepEndListener );
+			runner.on( 'end', endListener );
+
+			await runner.run();
+
+			expect( startListener, 'start' ).to.have.been.calledOnce;
+			expect( stepStartListener ).to.have.been.calledImmediatelyAfter( startListener );
+			expect( stepStartListener, 'step:start' ).to.have.been.calledOnce;
+			expect( stepEndListener ).to.have.been.calledImmediatelyAfter( stepStartListener );
+			expect( stepEndListener, 'step:end' ).to.have.been.calledOnce;
+			expect( endListener ).to.have.been.calledImmediatelyAfter( stepEndListener );
+			expect( endListener, 'end' ).to.have.been.calledOnce;
+		} );
+
+		describe( 'step:*', () => {
+			it( 'provides info about the step', async () => {
+				const runner = new Runner();
+				const resultsTemplate = {
+					results: {
+						ok: true
+					},
+					reporter() {}
+				};
+				const results1 = { ...resultsTemplate };
+				const results2 = { ...resultsTemplate };
+				const steps = [
+					{
+						name: 'Step #1',
+						run() {
+							return results1;
+						}
+					},
+
+					{
+						name: 'Step #2',
+						run() {
+							return results2;
+						}
+					}
+				];
+
+				runner.addSteps( steps );
+
+				const startListener = spy();
+				const endListener = spy();
+
+				runner.on( 'step:start', startListener );
+				runner.on( 'step:end', endListener );
+
+				await runner.run();
+
+				expect( startListener ).to.have.been.calledWithExactly( steps[ 0 ] );
+				expect( startListener ).to.have.been.calledWithExactly( steps[ 1 ] );
+				expect( endListener ).to.have.been.calledWithExactly( steps[ 0 ], results1 );
+				expect( endListener ).to.have.been.calledWithExactly( steps[ 1 ], results2 );
+			} );
+		} );
+
+		describe( 'end', () => {
+			it( 'is not emitted when error occurs during step', async () => {
+				const step = {
+					name: 'Step',
+					run() {
+						throw new Error( 'Some error' );
+					}
+				};
+				const runner = new Runner();
+				const endListener = spy();
+
+				runner.addStep( step );
+
+				runner.on( 'end', endListener );
+
+				try {
+					await runner.run();
+				} catch {} // eslint-disable-line no-empty
+
+				expect( endListener ).not.to.have.been.called;
+			} );
+		} );
+
+		describe( 'error', () => {
+			it( 'is emitted when error occurs during step', async () => {
+				const error = new Error( 'Some error' );
+				const step = {
+					name: 'Step',
+					run() {
+						throw error;
+					}
+				};
+				const runner = new Runner();
+				const errorListener = spy();
+
+				runner.addStep( step );
+
+				runner.on( 'error', errorListener );
+
+				try {
+					await runner.run();
+				} catch {} // eslint-disable-line no-empty
+
+				expect( errorListener ).to.have.been.calledOnce;
+				expect( errorListener ).to.have.been.calledWithExactly( error );
+			} );
+
+			it( 'is emitted when step returns incorrect results', async () => {
+				const step = {
+					name: 'Step',
+					run() {
+						return;
+					}
+				};
+				const runner = new Runner();
+				const errorListener = spy();
+
+				runner.addStep( step );
+
+				runner.on( 'error', errorListener );
+
+				try {
+					await runner.run();
+				} catch {} // eslint-disable-line no-empty
+
+				expect( errorListener ).to.have.been.calledOnce;
+
+				const [ error ] = errorListener.getCall( 0 ).args;
+
+				expect( error ).to.be.an.instanceOf( TypeError );
+				expect( error.message ).to.equal( 'Step Step didn\'t return correct results' );
+			} );
 		} );
 	} );
 } );
