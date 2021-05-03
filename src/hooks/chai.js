717e5d8516b5b658cd2c2a3aa79b816d5e90ab69
Comandeer <vepomoc@gmail.com>
2021-05-04 01:02:35 +0200

    refactor(hooks): update preamble in chai hook

--- src/hooks/chai.js
+++ src/hooks/chai.js
@@ -1,8 +1,13 @@
 import { resolve as resolvePath } from 'path';
 import { addHook } from 'pirates';
 const chaiPreamble = 'import{expect}from"chai";import{use as chaiUse}from"chai";import sinon from"sinon";import chaiAsPromised from"chai-as-promised";import sinonChai from"sinon-chai";import{noCallThru as pqNoCallThru}from"proxyquire";chaiUse(chaiAsPromised),chaiUse(sinonChai);const proxyquire=pqNoCallThru();';
+let added = false;
 
 function addChaiHook( projectPath ) {
+	if ( added ) {
+		return;
+	}
+
 	const testsPath = resolvePath( projectPath, 'tests' );
 	const fixturesPath = resolvePath( testsPath, 'fixtures' );
 	addHook( ( code ) => {
@@ -15,6 +20,7 @@ function addChaiHook( projectPath ) {
 		}
 
 	} );
+	added = truel;
 }
 
 export default addChaiHook;
