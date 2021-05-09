3732fb617488d8de589502ca4347ee2765c347a1
Comandeer <vepomoc@gmail.com>
2021-05-09 13:14:27 +0200

    test(mlt): add tests for --watch flag

---
+++ tests/helpers/spawnCLI.js
@@ -0,0 +1,54 @@
+import { resolve as resolvePath } from 'path';
+import spawn from 'cross-spawn';
+
+const mlt = resolvePath( __dirname, '..', '..', 'bin', 'mlt' );
+
+function spawnCLI( projectPath, {
+	args = [],
+	delayedCallback = {},
+	killAfter = null
+} = {} ) {
+	return new Promise( ( resolve, reject ) => {
+		let stdout = '';
+		let stderr = '';
+		const mltProcess = spawn( mlt, args, {
+			cwd: projectPath,
+			env: Object.assign( {}, process.env, {
+				// We don't want any garbage in codecov reports.
+				NO_CODECOV: true
+			} )
+		} );
+
+		mltProcess.on( 'error', reject );
+
+		mltProcess.stdout.on( 'data', ( data ) => {
+			stdout += data;
+		} );
+
+		mltProcess.stderr.on( 'data', ( data ) => {
+			stderr += data;
+		} );
+
+		mltProcess.on( 'exit', ( code ) => {
+			resolve( {
+				stdout,
+				stderr,
+				exitCode: code
+			} );
+		} );
+
+		if ( delayedCallback.callback ) {
+			setTimeout( () => {
+				delayedCallback.callback( mltProcess );
+			}, delayedCallback.timeout || 0 );
+		}
+
+		if ( typeof killAfter === 'number' ) {
+			setTimeout( () => {
+				mltProcess.kill( 'SIGINT' );
+			}, killAfter );
+		}
+	} );
+}
+
+export default spawnCLI;
--- tests/mlt.js
+++ tests/mlt.js
@@ -1,5 +1,7 @@
 import { resolve as resolvePath } from 'path';
+import touch from 'touch';
 import executeCLI from './helpers/executeCLI.js';
+import spawnCLI from './helpers/spawnCLI.js';
 
 describe( 'mlt', () => {
 	it( 'executes tests in provided CWD and dumps results into stdout (ESM)', async () => {
@@ -89,5 +91,109 @@ describe( 'mlt', () => {
 		expect( stderr ).to.match( /Provided step names \("hublabubla", "whatever"\) are incorrect/, 'stderr is empty' );
 
 		expect( exitCode ).to.equal( 1 );
+	} );
+
+	// #57
+	describe( '--watch', () => {
+		it( 'does not terminate after full run', async () => {
+			const validProject = resolvePath( __dirname, 'fixtures', 'testsPackageValid' );
+			const { exitCode } = await spawnCLI( validProject, {
+				args: [ '--watch' ],
+				killAfter: 1500
+			} );
+
+			// If process is killed, then it does not return exit code.
+			expect( exitCode ).to.equal( null );
+		} );
+
+		it( 'runs all steps except CodeCov', async () => {
+			const validProject = resolvePath( __dirname, 'fixtures', 'testsPackageValid' );
+			const { stdout, stderr, exitCode } = await spawnCLI( validProject, {
+				args: [ '--watch' ],
+				killAfter: 5000
+			} );
+
+			expect( stdout ).to.match( /---Linter---/, 'linter step is visible in the output' );
+			expect( stdout ).to.match( /---Tester---/, 'tester step is visible in the output' );
+			expect( stdout ).to.match( /---Code Coverage---/, 'code coverage step is visible in the output' );
+			expect( stdout ).not.to.match( /---CodeCov---/, 'codecov step is not visible in the output' );
+
+			expect( stderr ).to.equal( '', 'stderr is empty' );
+
+			// If process is killed, then it does not return exit code.
+			expect( exitCode ).to.equal( null );
+		} );
+
+		it( 'skips subsequent steps when something fails but without process termination', async () => {
+			const validProject = resolvePath( __dirname, 'fixtures', 'testsPackageESM' );
+			const { stdout, stderr, exitCode } = await spawnCLI( validProject, {
+				args: [ '--watch' ],
+				killAfter: 5000
+			} );
+
+			expect( stdout ).to.match( /---Linter---/, 'linter step is visible in the output' );
+			expect( stdout ).to.match( /---Tester---/, 'tester step is visible in the output' );
+			expect( stdout ).not.to.match( /---Code Coverage---/, 'code coverage step is not visible in the output' );
+			expect( stdout ).not.to.match( /---CodeCov---/, 'codecov step is not visible in the output' );
+
+			expect( stderr ).to.match( /Step Tester/, 'stderr shows that the tester step failed' );
+
+			// If process is killed, then it does not return exit code.
+			expect( exitCode ).to.equal( null );
+		} );
+
+		it( 'reruns all steps except CodeCov if .js file is modified in the project src', async () => {
+			const validProject = resolvePath( __dirname, 'fixtures', 'testsPackageValid' );
+			const touchablePath = resolvePath( validProject, 'src', 'index.js' );
+			const { stdout, stderr, exitCode } = await spawnCLI( validProject, {
+				args: [ '--watch' ],
+				killAfter: 10000,
+				delayedCallback: {
+					callback() {
+						touch( touchablePath, {
+							force: true
+						} );
+					},
+					timeout: 5000
+				}
+			} );
+
+			expect( stdout ).to.match( /---Linter---.+?---Linter---/s, 'linter step is visible twice in the output' );
+			expect( stdout ).to.match( /---Tester---.+?---Tester---/s, 'tester step is visible twice in the output' );
+			expect( stdout ).to.match( /---Code Coverage---.+?---Code Coverage---/s, 'code coverage step is visible twice in the output' );
+			expect( stdout ).not.to.match( /---CodeCov---/, 'codecov step is not visible in the output' );
+
+			expect( stderr ).to.equal( '', 'stderr is empty' );
+
+			// If process is killed, then it does not return exit code.
+			expect( exitCode ).to.equal( null );
+		} );
+
+		it( 'reruns all steps except CodeCov if .js file is modified in the project tests', async () => {
+			const validProject = resolvePath( __dirname, 'fixtures', 'testsPackageValid' );
+			const touchablePath = resolvePath( validProject, 'tests', 'index.js' );
+			const { stdout, stderr, exitCode } = await spawnCLI( validProject, {
+				args: [ '--watch' ],
+				killAfter: 10000,
+				delayedCallback: {
+					callback() {
+						touch( touchablePath, {
+							force: true
+						} );
+					},
+					timeout: 5000
+				}
+			} );
+
+			expect( stdout ).to.match( /---Linter---.+?---Linter---/s, 'linter step is visible twice in the output' );
+			expect( stdout ).to.match( /---Tester---.+?---Tester---/s, 'tester step is visible twice in the output' );
+			expect( stdout ).to.match( /---Code Coverage---.+?---Code Coverage---/s, 'code coverage step is visible twice in the output' );
+			expect( stdout ).not.to.match( /---CodeCov---/, 'codecov step is not visible in the output' );
+
+			expect( stderr ).to.equal( '', 'stderr is empty' );
+
+			// If process is killed, then it does not return exit code.
+			expect( exitCode ).to.equal( null );
+		} );
 	} );
 } );
