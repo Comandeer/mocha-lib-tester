232664c43f6e19f9c9d5f9406e1dad88872f3b5f
Comandeer <vepomoc@gmail.com>
2021-05-09 22:47:54 +0200

    feat(mlt): introduce watchable steps
    Only watchable steps can be used in --watch mode. This change is connected with a deeper refactor of
    the whole flow of the package: choosing steps for the run is moved to its own file, additionally the
    public API changed.
    
    BREAKING CHANGE: Changes in public API

--- src/mlt.js
+++ src/mlt.js
@@ -1,69 +1,21 @@
 /* istanbul ignore file */
-import linter from './linter.js';
-import tester from './tester.js';
-import codeCoverage from './codeCoverage.js';
-import codecov from './codecov.js';
+
 import Runner from './Runner.js';
 import Logger from './Logger.js';
+import prepareSteps from './prepareSteps.js';
 
-async function mlt( steps = [ 'lint', 'test', 'coverage', 'codecov' ] ) {
-	const projectPath = process.cwd();
-	const defaultSteps = [
-		{
-			id: 'lint',
-			name: 'Linter',
-			run() {
-				return linter( projectPath );
-			}
-		},
-
-		{
-			id: 'test',
-			name: 'Tester',
-			run() {
-				return tester( projectPath );
-			}
-		},
-
-		{
-			id: 'coverage',
-			name: 'Code Coverage',
-			run() {
-				return codeCoverage( projectPath, global.__mltCoverage__ );
-			}
-		},
-
-		{
-			id: 'codecov',
-			name: 'CodeCov',
-			run() {
-				return codecov( projectPath );
-			}
-		}
-	];
-	const filteredSteps = steps.map( ( id ) => {
-		const step = defaultSteps.find( ( { id: stepId } ) => {
-			return stepId === id;
-		} );
-
-		return step || id;
+async function mlt( projectPath, {
+	requestedSteps = [ 'lint', 'test', 'coverage', 'codecov' ],
+	isWatch = false
+} = {} ) {
+	const steps = prepareSteps( projectPath, {
+		requestedSteps,
+		isWatch
 	} );
-	const invalidSteps = filteredSteps.filter( ( step ) => {
-		return typeof step === 'string';
-	} );
-
-	if ( invalidSteps.length > 0 ) {
-		const stepNames = invalidSteps.map( ( step ) => {
-			return `"${ step }"`;
-		} ).join( ', ' );
-
-		throw new TypeError( `Provided step names (${ stepNames }) are incorrect` );
-	}
-
 	const runner = new Runner();
 
 	new Logger( runner );
-	runner.addSteps( filteredSteps );
+	runner.addSteps( steps );
 
 	const result = await runner.run();
 	const exitCode = result ? 0 : 1;
---
+++ src/prepareSteps.js
@@ -0,0 +1,71 @@
+import linter from './linter.js';
+import tester from './tester.js';
+import codeCoverage from './codeCoverage.js';
+import codecov from './codecov.js';
+
+function prepareSteps( projectPath, {
+	requestedSteps,
+	isWatch = false
+} = {} ) {
+	const defaultSteps = [
+		{
+			id: 'lint',
+			name: 'Linter',
+			watchable: true,
+			run() {
+				return linter( projectPath );
+			}
+		},
+
+		{
+			id: 'test',
+			name: 'Tester',
+			watchable: true,
+			run() {
+				return tester( projectPath );
+			}
+		},
+
+		{
+			id: 'coverage',
+			name: 'Code Coverage',
+			watchable: true,
+			run() {
+				return codeCoverage( projectPath, global.__mltCoverage__ );
+			}
+		},
+
+		{
+			id: 'codecov',
+			name: 'CodeCov',
+			watchable: false,
+			run() {
+				return codecov( projectPath );
+			}
+		}
+	];
+	const filteredSteps = requestedSteps.map( ( id ) => {
+		const step = defaultSteps.find( ( step ) => {
+			const isWatchable = !isWatch || ( isWatch && step.watchable );
+
+			return step.id === id && isWatchable;
+		} );
+
+		return step || id;
+	} );
+	const invalidSteps = filteredSteps.filter( ( step ) => {
+		return typeof step === 'string';
+	} );
+
+	if ( invalidSteps.length > 0 ) {
+		const stepNames = invalidSteps.map( ( step ) => {
+			return `"${ step }"`;
+		} ).join( ', ' );
+
+		throw new TypeError( `Provided step names (${ stepNames }) are incorrect` );
+	}
+
+	return filteredSteps;
+}
+
+export default prepareSteps;
