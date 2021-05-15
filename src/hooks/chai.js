55e2edaaa1c72ef4221da12b01b501090d1ab596
Comandeer <vepomoc@gmail.com>
2021-05-16 00:58:14 +0200

    fix(hooks/chai): add for every project

--- src/hooks/chai.js
+++ src/hooks/chai.js
@@ -1,13 +1,8 @@
 import { resolve as resolvePath } from 'path';
 import { addHook } from 'pirates';
 const chaiPreamble = 'import{expect}from"chai";import{use as chaiUse}from"chai";import sinon from"sinon";import chaiAsPromised from"chai-as-promised";import sinonChai from"sinon-chai";import{noCallThru as pqNoCallThru}from"proxyquire";chaiUse(chaiAsPromised),chaiUse(sinonChai);const proxyquire=pqNoCallThru();';
-let added = false;
 
 function addChaiHook( projectPath ) {
-	if ( added ) {
-		return;
-	}
-
 	const testsPath = resolvePath( projectPath, 'tests' );
 	const fixturesPath = resolvePath( testsPath, 'fixtures' );
 	addHook( ( code ) => {
@@ -20,7 +15,6 @@ function addChaiHook( projectPath ) {
 		}
 
 	} );
-	added = true;
 }
 
 export default addChaiHook;
