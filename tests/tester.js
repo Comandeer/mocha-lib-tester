5f9a387bbf91f7a03b9f643ea4027577b0c371bf
Comandeer <vepomoc@gmail.com>
2021-11-11 14:33:38 +0100

    test(tester): make tests account for async API

--- tests/tester.js
+++ tests/tester.js
@@ -1,5 +1,5 @@
 import { join as joinPath } from 'path';
-import assertParameter from './helpers/assertParameter.js';
+import assertAsyncParameter from './helpers/assertAsyncParameter.js';
 import validateResults from './helpers/validateResults.js';
 import tester from '../src/tester.js';
 
@@ -25,7 +25,7 @@ describe( 'tester', () => {
 	} );
 
 	it( 'expects path as a first parameter', () => {
-		assertParameter( {
+		assertAsyncParameter( {
 			invalids: [
 				undefined,
 				null,
@@ -42,7 +42,7 @@ describe( 'tester', () => {
 				message: 'Provided path must be a non-empty string'
 			},
 			code( param ) {
-				tester( param );
+				return tester( param );
 			}
 		} );
 	} );
