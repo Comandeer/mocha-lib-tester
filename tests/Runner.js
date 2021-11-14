b71bd57015ad9e5a20e8a969319cbb56d7884512
Comandeer <vepomoc@gmail.com>
2021-11-14 23:51:21 +0100

    test(runner): update tests

--- tests/Runner.js
+++ tests/Runner.js
@@ -10,6 +10,16 @@ import EventEmitter from '../src/EventEmitter.js';
 const { spy, stub } = sinon;
 
 describe( 'Runner', () => {
+	let sinonSandbox;
+
+	beforeEach( () => {
+		sinonSandbox = sinon.createSandbox();
+	} );
+
+	afterEach( () => {
+		sinonSandbox.restore();
+	} );
+
 	it( 'is a class', () => {
 		expect( Runner ).to.be.a( 'function' );
 	} );
@@ -579,7 +589,7 @@ describe( 'Runner', () => {
 			expect( passedResults ).to.deep.equal( requiredStepsResults );
 		} );
 
-		it( 'throws when steps return invalid results', async () => {
+		it( 'returns false and emits error event when steps return invalid results', async () => {
 			const runner = new Runner();
 			const step = {
 				id: 'step',
@@ -587,16 +597,23 @@ describe( 'Runner', () => {
 				run() {},
 				report() {}
 			};
+			const listener = sinonSandbox.spy();
 
 			runner.addStep( step );
 
-			const result = runner.run();
+			runner.once( 'error', listener );
+			const result = await runner.run();
 
-			return expect( result ).to.eventually.be.rejectedWith( TypeError,
-				`Step ${ step.name } didn't return correct results` );
+			expect( result ).to.equal( false );
+			expect( listener ).to.have.been.calledOnce;
+
+			const error = listener.getCall( 0 ).args[ 0 ];
+
+			expect( error ).to.be.an.instanceOf( TypeError );
+			expect( error.message ).to.equal( `Step ${ step.name } didn't return correct results` );
 		} );
 
-		it( 'throws on error during the step', async () => {
+		it( 'returns false and emits error event on error during the step', async () => {
 			const runner = new Runner();
 			const step = {
 				id: 'step',
@@ -606,15 +623,23 @@ describe( 'Runner', () => {
 				},
 				report() {}
 			};
+			const listener = sinonSandbox.spy();
 
 			runner.addStep( step );
 
-			const result = runner.run();
+			runner.once( 'error', listener );
+			const result = await runner.run();
 
-			return expect( result ).to.eventually.be.rejectedWith( Error, 'Thrown' );
+			expect( result ).to.equal( false );
+			expect( listener ).to.have.been.calledOnce;
+
+			const error = listener.getCall( 0 ).args[ 0 ];
+
+			expect( error ).to.be.an.instanceOf( Error );
+			expect( error.message ).to.equal( 'Thrown' );
 		} );
 
-		it( 'throws on rejection from the step', () => {
+		it( 'returns false and emits error event on rejection from the step', async () => {
 			const runner = new Runner();
 			const step = {
 				id: 'step',
@@ -624,12 +649,20 @@ describe( 'Runner', () => {
 				},
 				report() {}
 			};
+			const listener = sinonSandbox.spy();
 
 			runner.addStep( step );
 
-			const result = runner.run();
+			runner.once( 'error', listener );
+			const result = await runner.run();
 
-			return expect( result ).to.eventually.be.rejectedWith( Error, 'Reject' );
+			expect( result ).to.equal( false );
+			expect( listener ).to.have.been.calledOnce;
+
+			const error = listener.getCall( 0 ).args[ 0 ];
+
+			expect( error ).to.be.an.instanceOf( Error );
+			expect( error.message ).to.equal( 'Reject' );
 		} );
 
 		it( 'returns true only if all steps return ok results', async () => {
@@ -844,7 +877,7 @@ describe( 'Runner', () => {
 		} );
 
 		describe( 'end', () => {
-			it( 'is not emitted when error occurs during step', async () => {
+			it( 'is emitted when error occurs during step', async () => {
 				const step = {
 					id: 'step',
 					name: 'Step',
@@ -864,7 +897,7 @@ describe( 'Runner', () => {
 					await runner.run();
 				} catch {} // eslint-disable-line no-empty
 
-				expect( endListener ).not.to.have.been.called;
+				expect( endListener ).to.have.been.called;
 			} );
 		} );
 
