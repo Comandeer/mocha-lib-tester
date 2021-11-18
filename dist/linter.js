e64c6e1203de9c755bcb62eb744789a49ef08a8a
Comandeer <vepomoc@gmail.com>
2021-11-18 01:16:00 +0100

    chore(dist): clean after release [ci skip]

--- dist/linter.js
+++
@@ -1,6 +0,0 @@
-const { linter } = require( '@comandeer/mocha-lib-tester' );
-const { expose } = require( 'threads/worker' );
-
-expose( ( projectPath ) => {
-	return linter( projectPath );
-} );
--- dist/worker.js
+++
@@ -1,9 +0,0 @@
-const { parentPort, workerData } = require( 'worker_threads' );
-const { linter } = require( './mocha-lib-tester.js' );
-
-( async function() {
-	const projectPath = workerData;
-	const results = await linter( projectPath );
-
-	parentPort.postMessage( results );
-}() );
