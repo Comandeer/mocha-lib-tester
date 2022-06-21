3b03cd46deda312950524468f438a152a061f4f4
Comandeer <vepomoc@gmail.com>
2022-06-22 00:10:08 +0200

    fix(workers): fix dynamic requires in workers

--- src/workers/linter.js
+++ src/workers/linter.js
@@ -1,9 +1,5 @@
 const { expose } = require( 'threads/worker' );
-
-const isDistDirRegex = /[/\\]dist[/\\]workers$/;
-const isBuild = isDistDirRegex.test( __dirname );
-const linterPath = isBuild ? '../mocha-lib-tester.js' : '../index.js';
-const { linter } = require( linterPath );
+const { linter } = require( '../index.js' );
 
 expose( ( projectPath ) => {
 	return linter( projectPath );
